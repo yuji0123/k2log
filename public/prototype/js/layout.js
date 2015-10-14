66985b21df3fcc9663331e1ebe002bd09e52db92
Yuji Koketsu <kyuji7123.public@gmail.com>
2015-10-14 13:35:26 +0900

    topページデザインにMixItUpを導入

---
+++ public/prototype/js/layout.js
@@ -0,0 +1,37 @@
+$(function(){
+	$('#container').on('mixStart', function(e, state){
+		$(".mix").css({
+			display: "inline-block"
+		});
+		setSubLength(state.totalShow);
+	});
+	$('#container').mixItUp();
+	$('.articlelist_link').hover(
+		function(){
+			// console.log($(this).find('.articlelist_subcontent'));
+			$(this).stop().fadeTo('fast', 0.);
+		},
+		function(){
+			$(this).stop().fadeTo('fast', 1.0);
+		});
+});
+
+
+
+function setSubLength(numMainList){
+	var _mainLength = 0;
+	if(typeof numMainList == 'number'){
+		console.log('the parameter is number.');
+		_mainLength = ((numMainList+1) * 50) + numMainList+1;
+	}else{
+		var _tmpNumMainList = parseInt(numMainList);
+		_mainLength = ((_tmpNumMainList+1) * 50) + _tmpNumMainList+1;
+	}
+	console.log(_mainLength);
+	// メインカラムから、サブカラムのsortby分とtwitter分を差し引いた高さを求める
+	var _restLenth = _mainLength - (662+307);
+	$(".sublist_rest").css({
+		"height": _restLenth+"px",
+		"margin-bottom": "2px"
+	});
+}
